05f73f5ef8f9851beb25daf7274b2616f22fb75e
John Doe <jonathanludv@gmail.com>
2023-05-08 15:40:51 +0200

    add emp repo

--- SuperSaiyanProjekt/Services/EmployeeRepo.cs
+++ SuperSaiyanProjekt/Services/EmployeeRepo.cs
@@ -1,33 +1,64 @@
-﻿using Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using SuperSaiyanProjekt.Data;
 
 namespace SuperSaiyanProjekt.Services
 {
     public class EmployeeRepo : IRepository<Employee>
     {
 
-        public Task<Employee> Add(Employee entity)
+        private readonly AppDbContext _dbContext;
+
+        public EmployeeRepo(AppDbContext dbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
         }
 
-        public Task<Employee> Get(int id)
+        public async Task<Employee> Add(Employee entity)
         {
-            throw new NotImplementedException();
+            await _dbContext.Set<Employee>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
         }
 
-        public Task<IEnumerable<Employee>> GetAll()
+        public async Task<Employee> Get(int id)
         {
-            throw new NotImplementedException();
+            var Employee = await _dbContext.Set<Employee>().FirstOrDefaultAsync(p => p.EmployeeId == id);
+            return Employee;
         }
 
-        public Task<Employee> Remove(int id)
+        public async Task<IEnumerable<Employee>> GetAll()
         {
-            throw new NotImplementedException();
+            var Employees = await _dbContext.Set<Employee>().ToListAsync();
+            return Employees;
         }
 
-        public Task<Employee> Update(int id, Employee entity)
+        public async Task<Employee> Remove(int id)
         {
-            throw new NotImplementedException();
+            var Employee = await _dbContext.Set<Employee>().FirstOrDefaultAsync(p => p.EmployeeId == id);
+            if (Employee != null)
+            {
+                _dbContext.Set<Employee>().Remove(Employee);
+                await _dbContext.SaveChangesAsync();
+            }
+            return Employee;
         }
+
+        public async Task<Employee> Update(int id, Employee exampleEmployee)
+        {
+            var Employee = await _dbContext.Set<Employee>().FirstOrDefaultAsync(p => p.EmployeeId == id);
+            if (Employee != null)
+            {
+                Employee.FirstName = exampleEmployee.FirstName;
+                Employee.LastName = exampleEmployee.LastName;
+                Employee.Phone = exampleEmployee.Phone;
+                Employee.Age = exampleEmployee.Age;
+                Employee.HireDate = exampleEmployee.HireDate;
+                Employee.Projects = exampleEmployee.Projects;
+                Employee.TimeReps = exampleEmployee.TimeReps;
+            }
+            return Employee;
+        }
+
     }
 }
