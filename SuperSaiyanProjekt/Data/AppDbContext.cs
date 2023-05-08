7122d3bd4705338c1c98548e1b125b95e0cdab8b
John Doe <jonathanludv@gmail.com>
2023-05-08 15:19:47 +0200

    add context

---
+++ SuperSaiyanProjekt/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace SuperSaiyanProjekt.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<Project> projects { get; set; }
+        public DbSet<TimeReport> timereports { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
--- SuperSaiyanProjekt/Services/EmployeeRepo.cs
+++ SuperSaiyanProjekt/Services/EmployeeRepo.cs
@@ -1,6 +1,33 @@
-﻿namespace SuperSaiyanProjekt.Services
+﻿using Models;
+
+namespace SuperSaiyanProjekt.Services
 {
-    public class EmployeeRepo
+    public class EmployeeRepo : IRepository<Employee>
     {
+
+        public Task<Employee> Add(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Employee>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> Update(int id, Employee entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
