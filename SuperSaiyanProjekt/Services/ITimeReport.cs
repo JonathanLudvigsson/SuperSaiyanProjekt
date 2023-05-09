3a50bfcf2b7cc5f98b85b19212494625194005bd
John Doe <jonathanludv@gmail.com>
2023-05-09 14:03:47 +0200

    support for new method

--- SuperSaiyanProjekt/Services/ITimeReport.cs
+++ SuperSaiyanProjekt/Services/ITimeReport.cs
@@ -5,5 +5,6 @@ namespace SuperSaiyanProjekt.Services
     public interface ITimeReport : IRepository<TimeReport>
     {
         Task<TimeReport> GetHoursInWeek(int employeeid, int week);
+        Task<IEnumerable<TimeReport>> GetAllEmployeeTimeReports(int employeeid);
     }
 }
