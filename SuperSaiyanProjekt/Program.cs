ec38774b1a60a886027dd93ebdea8a3675416e29
Tim Nilsson <60481888+BKKinvader@users.noreply.github.com>
2023-05-09 11:07:30 +0200

    Add ProjectController

--- SuperSaiyanProjekt/Program.cs
+++ SuperSaiyanProjekt/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SuperSaiyanProjekt.Data;
+
 namespace SuperSaiyanProjekt
 {
     public class Program
@@ -12,6 +15,8 @@ namespace SuperSaiyanProjekt
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Connection")));
+
 
             var app = builder.Build();
 
