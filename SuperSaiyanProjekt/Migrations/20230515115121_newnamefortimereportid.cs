beaa32f2e638bb7d968da3467232952e21c3da22
John Doe <jonathanludv@gmail.com>
2023-05-15 13:51:29 +0200

    new name for time report id

---
+++ SuperSaiyanProjekt/Migrations/20230515115121_newnamefortimereportid.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperSaiyanProjekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class newnamefortimereportid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RepoId",
+                table: "timereports",
+                newName: "TimeReportId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TimeReportId",
+                table: "timereports",
+                newName: "RepoId");
+        }
+    }
+}
--- SuperSaiyanProjekt/Services/TimeReportRepo.cs
+++ SuperSaiyanProjekt/Services/TimeReportRepo.cs
@@ -22,7 +22,7 @@ namespace SuperSaiyanProjekt.Services
 
         public async Task<TimeReport> Get(int id)
         {
-            var TimeReport = await _dbContext.Set<TimeReport>().FirstOrDefaultAsync(p => p.RepoId == id);
+            var TimeReport = await _dbContext.Set<TimeReport>().FirstOrDefaultAsync(p => p.TimeReportId == id);
             return TimeReport;
         }
 
@@ -34,7 +34,7 @@ namespace SuperSaiyanProjekt.Services
 
         public async Task<TimeReport> Remove(int id)
         {
-            var TimeReport = await _dbContext.Set<TimeReport>().FirstOrDefaultAsync(p => p.RepoId == id);
+            var TimeReport = await _dbContext.Set<TimeReport>().FirstOrDefaultAsync(p => p.TimeReportId == id);
             if (TimeReport != null)
             {
                 _dbContext.Set<TimeReport>().Remove(TimeReport);
@@ -45,10 +45,10 @@ namespace SuperSaiyanProjekt.Services
 
         public async Task<TimeReport> Update(int id, TimeReport exampleTimeReport)
         {
-            var TimeReport = await _dbContext.Set<TimeReport>().FirstOrDefaultAsync(p => p.RepoId == id);
+            var TimeReport = await _dbContext.Set<TimeReport>().FirstOrDefaultAsync(p => p.TimeReportId == id);
             if (TimeReport != null)
             {
-                TimeReport.RepoId = exampleTimeReport.RepoId;
+                TimeReport.TimeReportId = exampleTimeReport.TimeReportId;
                 TimeReport.EmployeeId = exampleTimeReport.EmployeeId;
                 TimeReport.ProjectId = exampleTimeReport.ProjectId;
                 TimeReport.WeekNumber = exampleTimeReport.WeekNumber;
